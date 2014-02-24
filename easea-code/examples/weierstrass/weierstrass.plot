cb4a55993ddfe4410134ab7c7c074f6c5234172e
Pallamidessi <pallamidessi@etu.unistra.fr>
2014-02-24 16:05:40 +0100

    clean old file from repo

--- easea-code/examples/weierstrass/weierstrass.plot
+++
@@ -1,6 +0,0 @@
-set term png
-set output "weierstrass.png"
-set xrange[0:20480]
-set xlabel "Number of Evaluations"
-set ylabel "Fitness"
-plot 'weierstrass.dat' using 3:4 t 'Best Fitness' w lines, 'weierstrass.dat' using 3:5 t  'Average' w lines, 'weierstrass.dat' using 3:6 t 'StdDev' w lines
--- easea-code/examples/weierstrass/weierstrass.r
+++
@@ -1,9 +0,0 @@
-#Plotting for R
-png("weierstrass.png")
-data <- read.table("./weierstrass.csv",sep=",")
-plot(0, type = "n", main = "Plot Title", xlab = "Number of Evaluations", ylab = "Fitness", xlim = c(0,20480) )
-grid() # add grid
-lines(data[,3], data[,4], lty = 1) #draw first dataset
-lines(data[,3], data[,5], lty = 2) #draw second dataset
-lines(data[,3], data[,6], lty = 3) #draw third dataset
-legend("topright", c("Best Fitness", "Average", "StdDev"), lty = c(1, 2, 3) )
