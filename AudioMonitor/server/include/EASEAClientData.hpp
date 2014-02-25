fec58415458adce7622fdfd841c1d73b2bb90a87
Pallamidessi <pallamidessi@etu.unistra.fr>
2014-02-25 13:14:56 +0100

    Add doxygen type documentation

--- AudioMonitor/server/include/EASEAClientData.hpp
+++ AudioMonitor/server/include/EASEAClientData.hpp
@@ -23,29 +23,149 @@
 #include <cstdlib>
 #include <vector>
 
-/*For each node of an EASEA Cloud, store the received data*/
+/**
+*  /struct   EASEAClientData 
+*  /brief    Represent an EASEA client and store its data. 
+*  /details  Hold client data and socket. 
+*  
+**/
 class EASEAClientData {
   
   public:
+    
+    /*Constructor/Destructor------------------------------------------------------*/ 
+    /**
+    * /brief    Constructor of EASEAClientData
+    * /details  Empty constructor
+    *
+    **/
     EASEAClientData ();
+    
+    
+    /**
+    * /brief    Constructor of EASEAClientData
+    * /details Store the client's socket.
+    *
+    * @param  sock A previously created socket representing an EASEA client
+    **/
     EASEAClientData (int sock);
+    
+    
+    /**
+    * /brief    Destructor of EASEAClientData   
+    * /details 
+    *
+    **/
     virtual ~EASEAClientData ();
-    void processBuffer(char* buffer);
+    
+    /*Method----------------------------------------------------------------------*/
+    /**
+    * /brief    A more generic way to add data. 
+    * /details  See specific implementation for details.
+    *
+    *  @param  buffer Adress to the data to process
+    **/
+    virtual void processBuffer(char* buffer);
+    
+
+    /**
+    * /brief    Add data to the different vector.
+    * /details  TODO: Not very generic.
+    *
+    *  @param  best     Value to push back to the best's vector.
+    *  @param  worst    Value to push back to the worst's vector.
+    *  @param  stdev    Value to push back to the stdev's vector.
+    *  @param  average  Value to push back to the average's vector.
+    **/
     void addData(float best,float worst,float stdev,float average);
+    
+    
+    /**
+    * /brief    Print the content of the different vectors.
+    * /details  For debugging purpose, print on stdout.
+    *
+    **/
     void print();
-    /*getter*/
+
+    
+    /*Getter----------------------------------------------------------------------*/
+    /**
+    * /brief   Return the socket.
+    * /details Getter.
+    *
+    * @return  socket This instance's socket.
+    **/
     int getSocket();
+    
+    
+    /**
+    * /brief   Return the last data on each vector, in an array.
+    * /details TODO: dirty
+    *
+    * @return  array An flat array of the last data on each vector.
+    **/
     float* getLast(); //Vague: specialized struct instead ?
+    
+    
+    /**
+    * /brief    Return the value of ignoreFlag
+    * /details  The select based server check this to know if the data are wanted.
+    *
+    * @return  ignoreFlag The current ignore flag.
+    **/
     bool toIgnore();
+    
+    
+    /**
+    * /brief    Return the vector containing the worst notes.
+    * /details Getter.
+    *
+    * @return  vector A pointer to the vector.
+    **/
     std::vector<float>* getWorstVector();
+    
+    
+    /**
+    * /brief    Return the vector containing the best notes.
+    * /details Getter.
+    *
+    * @return  vector A pointer to the vector.
+    **/
     std::vector<float>* getBestVector();
+    
+    
+    /**
+    * /brief    Return the vector containing the standard deviation of the
+    * population.
+    * /details Getter.
+    *
+    * @return  vector A pointer to the vector.
+    **/
     std::vector<float>* getStDevVector();
+    
+    
+    /**
+    * /brief    Return the vector containing the average notes of the population.
+    * /details Getter.
+    *
+    * @return  vector A pointer to the vector.
+    **/
     std::vector<float>* getAverageVector();
-    /*setter*/
+    
+    
+    /*Setter----------------------------------------------------------------------*/
+    /**
+    * /brief    Set the ignore flag.
+    * /details  Data will not be process/add if ignoreFlag is true. 
+    *
+    *  @param  value New value of the ignoreFlag.
+    **/
     void setIgnoreFlag(bool value);
+  
+  
   private:
   
-    /* data */
+    /* Data-----------------------------------------------------------------------*/
     int clientSockfd;
     int nbData;
     bool ignoreFlag;
