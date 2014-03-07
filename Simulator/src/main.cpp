c93dcf9f38d95f23aeb7092ffe6baa116219ead7
Pallamidessi <joseph.pallamidessi@gmail.com>
2014-03-07 15:24:44 +0100

    Feature added : now take a directory and create a reader per .dat .csv

--- Simulator/src/main.cpp
+++ Simulator/src/main.cpp
@@ -15,8 +15,32 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details at
  * http://www.gnu.org/copyleft/gpl.html
-**/ 
+**/
+
 #include "main.hpp"
+
+namespace fs= boost::filesystem;
+
+bool checkDir(fs::path p,std::vector<Reader*> *listReader){
+  fs::directory_iterator end_iter;
+  
+  if (fs::exists(p) && fs::is_directory(p)) {
+    for (fs::directory_iterator dirIter(p); dirIter!=end_iter; dirIter++) {
+      if (fs::is_regular_file(dirIter->status())) {
+        if(dirIter->path().extension().compare(".dat")||
+           dirIter->path().extension().compare(".csv")){
+          std::string pp(dirIter->path().c_str());
+          listReader->push_back(new Reader(pp.c_str(),
+                               new AudioMonitorModule(),
+                               new ClientMonitorParameter(NULL)));  
+          
+        } 
+      }
+    }
+  return true;
+  }
+return false;
+}
 
 int main(int argc, char* argv[]){
   int nbOfReader=argc-2; 
@@ -25,15 +49,18 @@ int main(int argc, char* argv[]){
 
   if (argc<=2) {
     std::cerr<<
-      "Spécifier au moins un fichier pour lancer la simulation."
+      "Spécifier au moins un chemin pour lancer la simulation."
     <<std::endl;
     return 1;
   }
-  
-  std::string ip(argv[1]);
-  std::cout<<ip<<std::endl;
-  for (i = 0; i < nbOfReader; i++) {
-    listReader.push_back(new Reader(argv[i+2],new AudioMonitorModule(),new ClientMonitorParameter(NULL)));  
+ 
+  if (checkDir(argv[2],&listReader)) {
+    nbOfReader=listReader.size();
+  }
+  else {
+    for (i = 0; i < nbOfReader; i++) {
+      listReader.push_back(new Reader(argv[i+2],new AudioMonitorModule(),new ClientMonitorParameter(NULL)));  
+    }
   }
   
   for (i = 0; i < nbOfReader; i++) {
