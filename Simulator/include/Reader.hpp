0572c215df0091e4f752424a62b0953b85dac5be
Pallamidessi <joseph.pallamidessi@gmail.com>
2014-03-10 10:01:57 +0100

    Doc doxygen documentation to class Reader

--- Simulator/include/Reader.hpp
+++ Simulator/include/Reader.hpp
@@ -27,14 +27,59 @@
 #include <boost/filesystem/fstream.hpp>
 #include <boost/thread.hpp>
 
+/**
+*  /struct   Reader 
+*  /brief    Take an .csv .dat input file and simulated and EASEA node
+*  /details  Used the communication modules from EASEA programs. Each Reader
+*            object run in a separated thread.
+**/
 class Reader {
 public:
+  /**
+  * /brief    Constructor of Reader
+  * /details  Use a pre-existing AudioMonitorModule which initialized the
+  *           communication to the central server.
+  *
+  * @param    path          Path to a data file (currently supported : EASEA formated .csv and
+  *                         .dat)
+  * @param    sendingModule An AudioMonitorModule correctly linked to a monitoring server.
+  * @param    params        A MonitorParameter derived object
+  **/
   Reader (char* path,AudioMonitorModule* sendingModule,MonitorParameter* params);
+  /**
+  * /brief    Destructor of Reader
+  * /details  Free the associated AudioMonitorModule and MonitorParameter 
+  *
+  **/
   virtual ~Reader ();
   
+  /**
+  * /brief    Start the simulation in an encapsulated thread
+  * /details  Call readAndSend() in a boost::thread.
+  *
+  **/
   void start();
+  /**
+  * /brief    Join the encapsulated thread.
+  * /details  Wrap boost::thread join() on the encapsulated thread.
+  *
+  **/
   void join();
+  /**
+  * /brief    The main loop, read from the input file to send a
+  *           monitoring message.
+  * /details  
+  **/
   void readAndSend();
+  /**
+  * /brief    Test if the given buffer correspond to a data line or not
+  * /details  Check if '#' if the first letter of a .dat line, or if the first
+  *           element is a letter in case of a .csv file
+  *
+  * @param    extension The input file path extension 
+  * @param    buffer    A buffer corresponding to a line of the file 
+  * @return   bool      True if it's a data line, false otherwise.
+  **/
   bool testLine(boost::filesystem::path extension,std::string buffer);
 private:
   boost::filesystem::path mPathTo;
