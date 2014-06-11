2f3d26eb94f9b1267809f98196100d94a3119f6f
pallamidessi <joseph.pallamidessi@gmail.com>
2014-06-11 16:36:43 +0200

    Add starting ground for FluidCompositor

---
+++ AudioMonitor/server/include/FluidCompositor.hpp
@@ -0,0 +1,46 @@
+/**
+ * @file MIDICompositor.cpp
+ * @author Pallamidessi Joseph
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * http://www.gnu.org/copyleft/gpl.html
+**/  
+#ifndef MIDICOMPOSITOR_H__
+#define MIDICOMPOSITOR_H__
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include"Compositor.hpp"
+
+class MIDICompositor:public Compositor {
+  public:
+    MIDICompositor(std::string synthesisIP,int portnum,bool dbg=false);
+    virtual ~MIDICompositor ();
+    void send();
+    void send(osc::OutboundPacketStream oscMsg);
+    osc::OutboundPacketStream compose(EASEAClientData* cl);
+    void notify(EASEAClientData* cl);
+    void aSending(EASEAClientData* cl);
+    void aReception(EASEAClientData* cl);
+  
+  protected:
+    
+  private:
+    /* data */
+};
+
+#endif /* end of include guard: MIDICOMPOSITOR_H__ */
+
+
